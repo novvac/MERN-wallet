ee99ed037017b76832f5265806414b0355b71ad8
Novvac <novvac21@protonmail.com>
2023-03-20 22:09:20 +0100

    feat: Improve auth layout

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CssBaseline } from '@mui/material';
 import { RouterProvider } from 'react-router-dom';
 
 import router from './router';
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
--- client/src/layouts/auth.tsx
+++ client/src/layouts/auth.tsx
@@ -1,11 +1,56 @@
+import { styled } from '@mui/system';
+import { Stack, Grid } from '@mui/material';
 import { Outlet } from 'react-router-dom';
+
+const GridContainer = styled(Grid)(({ theme }) => ({
+    maxWidth: theme.breakpoints.values.xl,
+    height: '100vh',
+    maxHeight: 800,
+    margin: 'auto',
+}))
+
+const StepsBox = styled('div')({
+    background: '#194bfb',
+    color: 'white',
+    height: '100%',
+    position: 'sticky',
+    maxHeight: 'calc(100vh - 32px)',
+    top: 16
+})
 
 export default function AuthLayout() {
     return (
-        <div>
-            auth layout
+        <GridContainer
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+        >
+            <Grid item xs={12} md={4} sx={{ p: 2 }}>
+                <StepsBox sx={{ p: 3, width: 1 }}>Steps box</StepsBox>
+            </Grid>
 
-            <Outlet />
-        </div>
+            <Grid
+                item
+                xs={12}
+                md={8}
+                display="flex"
+                direction="column"
+                sx={{ pt: 5, px: 5 }}
+            >
+                <Outlet/>
+
+                <Stack
+                    direction="row"
+                    justifyContent="space-between"
+                    flex="none"
+                    position="sticky"
+                    bottom={0}
+                    sx={{ background: 'white', py: 5 }}
+                >
+                    <div>Privacy Policy</div>
+                    <div>Copyright 2023</div>
+                </Stack>
+            </Grid>
+        </GridContainer>
     )
 }
